528ab64c723c574369ffcd0eac848ae22ddc38ee
Rejaulh <rejaulassam@gmail.com>
2026-01-13 10:14:36 +0530

    create two componets(NewCollections and Newletter)

--- src/Components/Pages/Shop.js
+++ src/Components/Pages/Shop.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Hero/Hero'
 import Popular from '../Popular/Popular'
 import Offers from '../Offers/Offers'
+import NewCollections from '../NewCollections/NewCollections'
+import NewsLetter from '../NewsLetter/NewsLetter'
 
 const Shop = () => {
   return (
@@ -9,7 +11,8 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
-
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
