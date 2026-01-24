04bc4c59a59e2400fb54f448c4c59b3690d51dda
Rejaulh <rejaulassam@gmail.com>
2026-01-24 11:47:48 +0530

    Fix product routing and display components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-// import './App.css';
+import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Shop from './Components/Pages/Shop';
@@ -22,8 +22,8 @@ function App() {
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-        <Route path='/product' element={<Product/>}/>
-        <Route path='/productId' element={<Product/>}/>
+        {/* <Route path='/product' element={<Product/>}/> */}
+        <Route path='/product/:productId' element={<Product/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
--- src/Components/Pages/Product.js
+++ src/Components/Pages/Product.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Breadcrums/Breadcrum';
+import ProductDisplay from '../ProductDisply/ProductDisplay';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find(
+    (item) => item.id === Number(productId)
+  )
+  // if (!product) return null
+
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
+    </div>
   )
 }
 
